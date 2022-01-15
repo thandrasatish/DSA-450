2e384769a8ee446e06700e654273fa1c4864545a
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 22:44:41 +0530

    Reverse The Array

--- Reverse The Array/Iterative Way.py
+++
@@ -1,9 +0,0 @@
-def reversearray(A,start,end):
-    while(start <end):
-        A[start],A[end]=A[end],A[start]
-        start+=1
-        end-=1
-    
-A=list(map(int,input().split()))
-reversearray(A,0,len(A)-1)
-print(*A)
